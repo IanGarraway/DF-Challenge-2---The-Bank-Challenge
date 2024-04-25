8907455ad62fa91354cc95d59b0de38a6eef4a00
Ian Garraway <IJGarraway@gmail.com>
2024-04-25 17:36:40 +0100

    Feat: getCredit returning correct value

--- src/CreditTransaction.js
+++ src/CreditTransaction.js
@@ -2,7 +2,11 @@ import Transaction from "./Transaction.js";
 
 export default class CreditTransaction extends Transaction{
     constructor(date, value) {
-        super(date, value)
+        super(date, value, value, "");
+
+        //Transactions constructors properties are date, value, credit, debit
+        //I'd have preferred to have made them protected, but this should work
+        
     }
 
 }
--- src/Transaction.js
+++ src/Transaction.js
@@ -1,15 +1,20 @@
 export default class Transaction{
     #value;
+    #credit;
     
     
     
-    constructor(date, value){
+    constructor(date, value, credit, debit){
         if (new.target === Transaction) throw new TypeError("Cannot construct abstract instances directly");
         this.#value = value;
+        this.#credit = credit;
     
     }
 
     getValue() {
         return this.#value;
+    }
+    getCredit() {
+        return this.#credit;
     }
 }
