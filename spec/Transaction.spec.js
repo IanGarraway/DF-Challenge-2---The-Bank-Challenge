5941295a9e4f2972688221a6355444a027ca31f5
Ian Garraway <IJGarraway@gmail.com>
2024-04-26 17:36:59 +0100

    Test: add test to ensure getdate works on a debit transaction

--- spec/Transaction.spec.js
+++ spec/Transaction.spec.js
@@ -144,6 +144,16 @@ describe("Transaction Tests:", () => {
             expect(actual).toEqual(" ");
         });
 
+        it("Should return the date when getDate is called", () => {
+            //Arrange
+            const testTransaction = new DebitTransaction("25/04/2024", 750);
+            //Act
+            const actual = testTransaction.getDate();
+            //Assert
+            expect(actual).toEqual("25/04/2024");
+        });
+        
+
         
     }); // end of Debit transaction tests
     
