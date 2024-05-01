253d8dd4f51d68ddc4c8d36b48fa0f7b2b01a72f
Ian Garraway <IJGarraway@gmail.com>
2024-05-01 19:49:21 +0100

    Feat: add demonstration code to index.js to demonstrate developed features

--- src/index.js
+++ src/index.js
@@ -10,16 +10,56 @@ function loadAccount() {
     return new Account();
 }
 
+function separators() {
+    console.log();
+    console.log("-");
+    console.log();
+}
+
+function statementBlock() {
+    console.log("Statement: ");
+    console.log();
+}
+
 
 let theAccount = loadAccount();
 
 //adding transactions
-
+//credit 1000
+console.log("Client makes a deposit of 1000 on 10-01-2012");
 theAccount.addTransaction(new CreditTransaction("10/01/2012", 1000));
+statementBlock();
+Printer.printStatement(theAccount.getTransactions());
+separators();
+
+//credit 2000
+console.log("Client makes a deposit of 2000 on 13-01-2012");
 theAccount.addTransaction(new CreditTransaction("13/01/2012", 2000));
+statementBlock();
+
+Printer.printStatement(theAccount.getTransactions());
+separators();
+
+//debit 500
+console.log("Client makes a withdrawal of 500 on 14-01-2012");
 theAccount.addTransaction(new DebitTransaction("14/01/2012", 500));
 
-//requesting the bank statement
+statementBlock();
+//requesting the bank statement that meets the acceptance criteria
+Printer.printStatement(theAccount.getTransactions());
+
+separators();
+console.log("Client attempts to withdraw 3000 on the 16-01-2012");
+const result = theAccount.addTransaction(new DebitTransaction("16/01/2012", 3000));
+
+console.log(`And was informed: ${result}`);
+console.log("After speaking to a bank manager, they requested an overdraft of 1000, it was granted");
+theAccount.setOverdraft(true);
+theAccount.setOverdraftAmount(1000);
+console.log("So the next day they tried to withdraw the money again");
+theAccount.addTransaction(new DebitTransaction("17/01/2012", 3000))
+
+statementBlock();
 Printer.printStatement(theAccount.getTransactions());
 
 
